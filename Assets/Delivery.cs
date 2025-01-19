2e9a68d1cfd1e14fca61b00b98ed8f79aeb1142f
Taksanzu <ankhang897979@gmail.com>
2025-01-19 14:46:49 +0700

    add bool for check package

--- Assets/Delivery.cs
+++ Assets/Delivery.cs
@@ -1,18 +1,30 @@
 using System.Collections;
 using System.Collections.Generic;
+using JetBrains.Annotations;
 using UnityEngine;
 
 public class Delivery : MonoBehaviour
 {
+   private bool hasPackage;
+
+   private void Start()
+   {
+      hasPackage = false;
+   }
+
    private void OnTriggerEnter2D(Collider2D other)
    {
+
       if(other.tag == "Package")
       {
         Destroy(other.gameObject);
         Debug.Log("Package Delivered");
-      }else if(other.tag == "Customer")
+        hasPackage = true;
+      }
+      if(other.tag == "Customer" && hasPackage)
       {
         Debug.Log("Customer Delivered");
+        hasPackage = false;
       }
    }
 }
