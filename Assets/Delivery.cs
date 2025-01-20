1fd52c792ebc4100f15722077bebcd9990bc22bd
Taksanzu <ankhang897979@gmail.com>
2025-01-20 10:50:41 +0700

    change color when have package

--- Assets/Delivery.cs
+++ Assets/Delivery.cs
@@ -5,11 +5,20 @@ using UnityEngine;
 
 public class Delivery : MonoBehaviour
 {
+   [SerializeField] Color32 hasPackageColor = new Color32(0, 255, 0, 255);
+   [SerializeField] Color32 noPackageColor = new Color32(1, 1, 1, 1);
+
    private bool hasPackage;
+
+   SpriteRenderer spriteRenderer;
+
 
    private void Start()
    {
       hasPackage = false;
+      spriteRenderer = GetComponent<SpriteRenderer>();
+      spriteRenderer.color = noPackageColor;
+   
    }
 
    private void OnTriggerEnter2D(Collider2D other)
@@ -20,11 +29,13 @@ public class Delivery : MonoBehaviour
         Debug.Log("Package Delivered");
         hasPackage = true;
         Destroy(other.gameObject, 0.05f);
+        spriteRenderer.color = hasPackageColor;
       }
       if(other.tag == "Customer" && hasPackage)
       {
         Debug.Log("Customer Delivered");
         hasPackage = false;
+         spriteRenderer.color = noPackageColor;
       }
    }
 }
