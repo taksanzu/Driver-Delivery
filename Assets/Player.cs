1a2a266dfba4ac114a7f6c4b63742c7f5b87f475
Taksanzu <ankhang897979@gmail.com>
2025-01-21 21:08:46 +0700

    add speed up and slow down point

--- Assets/Player.cs
+++ Assets/Player.cs
@@ -1,11 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class Player : MonoBehaviour
 {
     private float speed = 20.0f;
     private float turnSpeed = 75.0f; 
+    private float speedUp = 30.0f;
+    private float slowDown = 10.0f;
     void Start()
     {
         
@@ -19,5 +22,24 @@ public class Player : MonoBehaviour
 
         transform.Translate(forwardInput * speed * Time.deltaTime * Vector3.up);
         transform.Rotate(-turnSpeed * turnInput * Time.deltaTime * Vector3.forward);
+    }
+
+    void OnCollisionEnter2D(Collision2D other)
+    {
+        speed = 20.0f;
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("SpeedUp"))
+        {
+            speed = speedUp;
+            Destroy(other.gameObject);
+        }
+        if (other.CompareTag("SlowDown"))
+        {
+            speed = slowDown;
+            Destroy(other.gameObject);
+        }
     }
 }
