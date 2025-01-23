3a872d413a6dbc014f4d7f528fc9a9a7ef350464
Taksanzu <ankhang897979@gmail.com>
2025-01-23 11:17:36 +0700

    add slow down
    slow down player when collide with world object

--- Assets/Player.cs
+++ Assets/Player.cs
@@ -5,20 +5,22 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    private float speed = 20.0f;
-    private float turnSpeed = 75.0f; 
+    private float speed;
+    private float normalSpeed = 20.0f;
+    private float turnSpeed = 100.0f; 
     private float speedUp = 30.0f;
     private float slowDown = 10.0f;
+    private float slowDuration = 2.0f;
+    private bool isSlowed = false;
+
     void Start()
     {
-        
+        speed = normalSpeed;
     }
-
     void Update()
     {
         float forwardInput = Input.GetAxis("Vertical");
         float turnInput = Input.GetAxis("Horizontal");
-
 
         transform.Translate(forwardInput * speed * Time.deltaTime * Vector3.up);
         transform.Rotate(-turnSpeed * turnInput * Time.deltaTime * Vector3.forward);
@@ -26,7 +28,22 @@ public class Player : MonoBehaviour
 
     void OnCollisionEnter2D(Collision2D other)
     {
-        speed = 20.0f;
+        if(other.gameObject.CompareTag("WorldObject") && !isSlowed)
+        {
+            StartCoroutine(SlowDown());
+        }
+        
+    }
+
+    IEnumerator SlowDown()
+    {
+        isSlowed = true;
+        speed = slowDown;
+
+        yield return new WaitForSeconds(slowDuration);
+
+        speed = normalSpeed;
+        isSlowed = false;
     }
 
     void OnTriggerEnter2D(Collider2D other)
